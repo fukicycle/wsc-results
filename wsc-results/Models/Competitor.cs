5b1dc618fe2aea9e0111e0592de3e055959193d4
Fuki Sato <106070646+fukicycle@users.noreply.github.com>
2023-06-20 10:34:42 +0900

    Display sample date (Get from wsc api)

--- wsc-results/Models/Competitor.cs
+++ wsc-results/Models/Competitor.cs
@@ -1,13 +1,16 @@
 ﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace wsc_results.Models
 {
     public class Competitor
     {
         [JsonProperty("first_name")]
+        [JsonPropertyName("first_name")]
         public string FirstName { get; set; }
 
         [JsonProperty("last_name")]
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
 
         [JsonProperty("person_id")]
